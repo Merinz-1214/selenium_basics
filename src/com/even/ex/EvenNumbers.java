10f461616ac33f369994e1fc7b95ba355b30dd2a
Merin <merintvarghese19@gmail.com>
2023-07-26 10:19:07 -0700

    initial commit

---
+++ src/com/even/ex/EvenNumbers.java
@@ -0,0 +1,13 @@
+package com.even.ex;
+
+public class EvenNumbers {
+    public static void main(String[] args) {
+        for (int i = 0;i<=20;i++){
+            int j =2;
+            if(i % j == 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
