3662b4c6f11acecde81c3e10a8bdad845e8c17d2
Merin <merintvarghese19@gmail.com>
2023-07-26 10:34:43 -0700

    New changes

--- src/com/even/ex/EvenNumbers.java
+++ src/com/even/ex/EvenNumbers.java
@@ -2,7 +2,7 @@ package com.even.ex;
 
 public class EvenNumbers {
     public static void main(String[] args) {
-        for (int i = 0;i<=20;i++){
+        for (int i = 0;i<=30;i++){
             int j =2;
             if(i % j == 0){
                 System.out.println(i);
